ecbd574cf62ece6eca41b1001244cc524213f577
Jaume Delclòs Coll <cosarara@cosarara.me>
2020-10-29 16:05:57 +0100

    atom feed support

--- setup.py
+++ setup.py
@@ -20,7 +20,8 @@ setup(
         'inflect',
         'mistune',
         'colorama',
-        'six'
+        'six',
+        'feedgen',
     ],
     include_package_data = True,
     entry_points = {
